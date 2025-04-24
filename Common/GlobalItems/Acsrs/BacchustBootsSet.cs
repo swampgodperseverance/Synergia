97237e6523d9280bf889b8474a960ea0370feff4
swampgodperseverance <kirillefsh@gmail.com>
2025-04-24 15:07:12 +0300

    Update BacchustBootsSet.cs

--- Common/GlobalItems/Acsrs/BacchustBootsSet.cs
+++ Common/GlobalItems/Acsrs/BacchustBootsSet.cs
@@ -2,28 +2,28 @@ using Terraria;
 using Terraria.ModLoader;
 using System.Collections.Generic;
 
-namespace Vanilla.Common.GlobalItems.Accessories
+namespace Vanilla.Common.GlobalItems.Acsrs
 {
 	public class BacchusBootsEdit : GlobalItem
 	{
 		public override bool AppliesToEntity(Item item, bool lateInstantiation)
 		{
-			// Проверяем, что это предмет из мода Avalon с нужным названием
+			// find this boots
 			return item.ModItem?.Mod?.Name == "Avalon" && item.ModItem?.Name == "BacchusBoots";
 		}
 
 		public override void UpdateAccessory(Item item, Player player, bool hideVisual)
 		{
-			// Добавляем +18% урона призывателя
+				// increasing damage  +10%
 			player.GetDamage(DamageClass.Summon) += 0.10f;
 		}
 
 		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
 		{
-			// Удаляем старую строку с +8%
+			// Remove old toltip
 			tooltips.RemoveAll(line => line.Text.Trim() == "8% increased summon damage");
 
-			// Добавляем новую строку
+			// 10+8=18, i'm not deleted old 8% damage
 			tooltips.Add(new TooltipLine(Mod, "VanillaBuffedSummon", "+18% summon damage")
 			{
 				OverrideColor = Microsoft.Xna.Framework.Color.LightGoldenrodYellow
