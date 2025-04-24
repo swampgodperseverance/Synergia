d817d29b0077553a6ae0e1db292a2dc642e3a629
Ropyxa <godzilaluka@gmail.com>
2025-04-24 12:53:49 +0300

    added gitignore

--- obj/Debug/net8.0/Vanilla.AssemblyInfo.cs
+++ obj/Debug/net8.0/Vanilla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vanilla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcd38d4304c6400a1981bb90a4350413e46bba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d2b966cd0bfba433870cca5f80ca2c6a6afb94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vanilla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vanilla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
