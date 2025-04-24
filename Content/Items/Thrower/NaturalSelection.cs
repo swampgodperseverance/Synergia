3e7af87fc056b1d0f0648e9e6832020cf4f98884
swampgodperseverance <kirillefsh@gmail.com>
2025-04-24 11:20:07 +0300

    Add files via upload

---
+++ Content/Items/Thrower/NaturalSelection.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Vanilla.Content.Proj;
+
+namespace Vanilla.Content.Thrower
+{
+    public class NaturalSelection : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 20;
+            Item.DamageType = DamageClass.Throwing;
+            Item.width = 32;
+            Item.height = 32;
+            Item.useTime = 25;
+            Item.useAnimation = 25;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 5;
+            Item.value = Item.buyPrice(gold: 1);
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item1;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<NaturalSelectionProj>();
+            Item.shootSpeed = 12f;
+        }
+    }
+}    
