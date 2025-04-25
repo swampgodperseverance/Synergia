7091d2cf6735bf9e989718ac5106dc7c6338f53e
swampgodperseverance <kirillefsh@gmail.com>
2025-04-25 20:49:42 +0300

    Add files via upload

---
+++ Content/Items/OldTales.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Vanilla.Content.Items
+{
+    public class OldTales : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Old Tales");
+            // Tooltip.SetDefault("Right-click to reveal the forgotten image...");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 30;
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override bool AltFunctionUse(Player player) => true;
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2 && Main.myPlayer == player.whoAmI)
+            {
+                if (!ModContent.GetInstance<OldTalesSystem>().UIVisible)
+                {
+                    ModContent.GetInstance<OldTalesSystem>().ShowUI();
+                }
+                return false;
+            }
+
+            return base.CanUseItem(player);
+        }
+    }
+}
