a7fdc3b90b3c73abd8e7b60ce73e8489a28d1d7d
Ropyxa <godzilaluka@gmail.com>
2025-04-25 12:32:35 +0300

    Throwing prefix fix

--- Common/GlobalItems/ThrowingConverter.cs
+++ Common/GlobalItems/ThrowingConverter.cs
@@ -1,102 +1,69 @@
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
+using System.Collections.Generic;
+using static Terraria.ModLoader.ModContent;
+
+using Consolaria.Content.Items.Weapons.Melee;
+using Consolaria.Content.Items.Weapons.Ranged;
+using Starforgedclassic.Content.Weapons.Solarang;
+using ValhallaMod.Items.Weapons.Thrown;
+using ValhallaMod.Items.Weapons.Glaive;
+using ValhallaMod.Items.Weapons.Javelin;
+using ValhallaMod.Items.Garden;
 
 namespace Vanilla.Common.GlobalItems
 {
-    public class ThrowingConverter : GlobalItem
-    {
-        public override void SetDefaults(Item item)
-        {
-            if (item.ModItem != null && item.ModItem.Mod.Name == "Avalon" && item.Name == "Crystal Tomahawk")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "Avalon" && item.Name == "Rotten Apple")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "Avalon" && item.Name == "Icicle")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "Avalon" && item.Name == "Shurikerang")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "Avalon" && item.Name == "Enchanted Shuriken")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Spider Knife")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Garlic")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "True Moon Cycle")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "True Light Disc")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Flesh Grinder")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Corruption's Servant")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Moon Cycle")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Chlorophyle Glaive")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Cursed Fire Coctail")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Cluster Beenade")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Ichor Coctail")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Sun Javelin")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Mekurabe")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Terra Glaive")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-             if (item.ModItem != null && item.ModItem.Mod.Name == "ValhallaMod" && item.Name == "Omega Disc")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "starforgedclassic" && item.Name == "Solarang")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-            if (item.ModItem != null && item.ModItem.Mod.Name == "Consolaria" && item.Name == "Spicy Sauce")
-            {
-                item.DamageType = DamageClass.Throwing;
-            }
-        }
-    }
+	public class VanillaThrowingConverter : GlobalItem
+	{
+		private static readonly HashSet<int> VanillaBoomerangs = new()
+		{
+			//Terraria
+			ItemID.EnchantedBoomerang,
+			ItemID.WoodenBoomerang,
+			ItemID.IceBoomerang,
+			ItemID.FruitcakeChakram,
+			ItemID.ThornChakram,
+			ItemID.BloodyMachete,
+			ItemID.Flamarang,
+			ItemID.LightDisc,
+			ItemID.Bananarang,
+			ItemID.PossessedHatchet,
+			ItemID.ScourgeoftheCorruptor,
+			ItemID.Anchor,
+			ItemID.Trimarang,
+			ItemID.Shroomerang,
+
+			//Consolaria
+			ItemType<AlbinoMandible>(),
+			ItemType<SpicySauce>(),
+
+			//Starforged Classic
+			ItemType<Solarang>(),
+
+			//ValhallaMod
+			ItemType<OmegaDisc>(),
+			ItemType<TerraGlaive>(),
+			ItemType<DungeonGlaive>(),
+			ItemType<SunJavelin>(),
+			ItemType<IchorCoctail>(),
+			ItemType<BigBeeNade>(),
+			ItemType<CursedCoctail>(),
+			ItemType<ChlorophyleGlaive>(),
+			ItemType<NightGlaive>(),
+			ItemType<CrimiteGlaive>(),
+			ItemType<TrueLightDisc>(),
+			ItemType<TrueNightGlaive>(),
+			ItemType<Garlic>(),
+			ItemType<SpiderKnife>()
+		};
+
+		public override void SetDefaults(Item item)
+		{
+			if (VanillaBoomerangs.Contains(item.type))
+			{
+				item.DamageType = DamageClass.Throwing;
+			}
+		}
+	}
 }
-    
--- Vanilla.cs
+++ Vanilla.cs
@@ -1,8 +1,27 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using System.Collections.Generic;
 
 namespace Vanilla
 {
-	public class Vanilla : Mod
+	public class Vanilla : GlobalItem
 	{
+		string internalName = "";
+
+		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+		{
+			if (item.ModItem != null)
+			{
+				internalName = item.ModItem.Name;
+			}
+			else
+			{
+			}
+
+			tooltips.Add(new TooltipLine(Mod, "InternalName", $"[i:{item.type}] [c/FFFF00:{internalName} (ID: {item.type})]"));
+		}
+
+		public override bool InstancePerEntity => true;
 	}
 }
