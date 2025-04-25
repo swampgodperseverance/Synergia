a638265dc446c2467be43f0625fcf3adf52bdf78
Ropyxa <godzilaluka@gmail.com>
2025-04-25 20:46:56 +0300

    Throwing prefix more #3

--- Common/GlobalItems/ThrowingConverter.cs
+++ Common/GlobalItems/ThrowingConverter.cs
@@ -2,7 +2,9 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using System.Collections.Generic;
+using Terraria.Localization;
 using static Terraria.ModLoader.ModContent;
+using static Terraria.Localization.Language;
 
 using Consolaria.Content.Items.Weapons.Melee;
 using Consolaria.Content.Items.Weapons.Ranged;
@@ -16,6 +18,7 @@ namespace Vanilla.Common.GlobalItems
 {
 	public class VanillaThrowingConverter : GlobalItem
 	{
+
 		private static readonly HashSet<int> VanillaBoomerangs = new()
 		{
 			//Terraria
@@ -70,9 +73,37 @@ namespace Vanilla.Common.GlobalItems
 			ItemType<CactusStar>()
 		};
 
+		private static Mod avalon = ModLoader.GetMod("Avalon");
+
 		public override void SetDefaults(Item item)
 		{
 			if (VanillaBoomerangs.Contains(item.type))
+			{
+				item.DamageType = DamageClass.Throwing;
+				return;
+			}
+
+			if (avalon != null && item.type == avalon.Find<ModItem>("TetanusChakram").Type)
+			{
+				item.DamageType = DamageClass.Throwing;
+			}
+			if (avalon != null && item.type == avalon.Find<ModItem>("Shurikerang").Type)
+			{
+				item.DamageType = DamageClass.Throwing;
+			}
+			if (avalon != null && item.type == avalon.Find<ModItem>("EnchantedShuriken").Type)
+			{
+				item.DamageType = DamageClass.Throwing;
+			}
+			if (avalon != null && item.type == avalon.Find<ModItem>("RottenApple").Type)
+			{
+				item.DamageType = DamageClass.Throwing;
+			}
+			if (avalon != null && item.type == avalon.Find<ModItem>("CrystalTomahawk").Type)
+			{
+				item.DamageType = DamageClass.Throwing;
+			}
+			if (avalon != null && item.type == avalon.Find<ModItem>("Icicle").Type)
 			{
 				item.DamageType = DamageClass.Throwing;
 			}
