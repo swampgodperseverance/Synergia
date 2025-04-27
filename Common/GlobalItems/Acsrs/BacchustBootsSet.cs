c547029718316e79e3b90af27125636dd00efbd3
Ropyxa <godzilaluka@gmail.com>
2025-04-27 13:31:55 +0300

    Bacchust Boots Set change

--- Common/GlobalItems/Acsrs/BacchustBootsSet.cs
+++ Common/GlobalItems/Acsrs/BacchustBootsSet.cs
@@ -6,16 +6,14 @@ namespace Vanilla.Common.GlobalItems.Acsrs
 {
 	public class BacchusBootsEdit : GlobalItem
 	{
-		public override bool AppliesToEntity(Item item, bool lateInstantiation)
-		{
-			// find this boots
-			return item.ModItem?.Mod?.Name == "Avalon" && item.ModItem?.Name == "BacchusBoots";
-		}
+		private static Mod avalon = ModLoader.GetMod("Avalon");
 
 		public override void UpdateAccessory(Item item, Player player, bool hideVisual)
 		{
-				// increasing damage  +10%
-			player.GetDamage(DamageClass.Summon) += 0.10f;
+			if (item.type == avalon.Find<ModItem>("BacchusBoots").Type)
+			{
+				player.GetDamage(DamageClass.Summon) += 0.10f; // increasing damage  +10%
+			}
 		}
 
 		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
@@ -24,7 +22,7 @@ namespace Vanilla.Common.GlobalItems.Acsrs
 			tooltips.RemoveAll(line => line.Text.Trim() == "8% increased summon damage");
 
 			// 10+8=18, i'm not deleted old 8% damage
-			tooltips.Add(new TooltipLine(Mod, "VanillaBuffedSummon", "+18% summon damage")
+			tooltips.Add(new TooltipLine(Mod, "VanillaBuffedSummon", "18% increased summon damage")
 			{
 				OverrideColor = Microsoft.Xna.Framework.Color.LightGoldenrodYellow
 			});
