2a9acdffd69e3b1c832674557e67b8f446dabe33
Ropyxa <godzilaluka@gmail.com>
2025-04-27 12:42:13 +0300

    Cursed Throw Emblem

---
+++ Content/Items/Accessories/CursedThrowEmblem.cs
@@ -0,0 +1,31 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace Vanilla.Content.Items.Accessories
+{
+	public class CursedThrowEmblem : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.RegisterItemAnimation(this.Item.type, (DrawAnimation) new DrawAnimationVertical(5, 5, false));
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 32;
+			Item.height = 32;
+			Item.accessory = true;
+			Item.value = Item.sellPrice(gold: 17);
+			Item.rare = ItemRarityID.Red;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.GetDamage(DamageClass.Throwing) += 0.75f;
+			player.GetDamage(DamageClass.Generic) -= 0.5f;
+		}
+	}
+}
