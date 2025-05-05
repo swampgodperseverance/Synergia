94a75cffcc9007681733c08e5649b56a0ebd4857
swampgodperseverance <kirillefsh@gmail.com>
2025-05-05 17:34:58 +0200

    Add files via upload

---
+++ Content/Projectiles/StoneMark.cs
@@ -0,0 +1,60 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Vanilla.Content.Projectiles
+{
+    public class StoneMark : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Stone Warning Mark");
+            Main.projFrames[Projectile.type] = 5;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.timeLeft = 60; 
+            Projectile.friendly = false;
+            Projectile.hostile = false;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.ignoreWater = true;
+            Projectile.aiStyle = 0;
+        }
+
+        public override void AI()
+        {
+            // animation
+            if (++Projectile.frameCounter >= 5) 
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame++;
+                if (Projectile.frame >= Main.projFrames[Projectile.type])
+                {
+                    Projectile.frame = 0;
+                }
+            }
+
+            // A little shake
+            Projectile.position += new Vector2(Main.rand.NextFloat(-0.2f, 0.2f), Main.rand.NextFloat(-0.2f, 0.2f));
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+            int frameHeight = texture.Height / Main.projFrames[Projectile.type];
+            Rectangle frame = new Rectangle(0, frameHeight * Projectile.frame, texture.Width, frameHeight);
+
+            Vector2 origin = frame.Size() / 2f;
+
+            Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, frame, Color.White, 0f, origin, 1f, SpriteEffects.None, 0);
+
+            return false;
+        }
+    }
+}
