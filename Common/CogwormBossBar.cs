7de20a6b8fb2b5a104ffacc647429a19e57c4e3e
swampgodperseverance <kirillefsh@gmail.com>
2025-08-04 11:43:39 +0200

    Add files via upload

---
+++ Common/CogwormBossBar.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace Vanilla.Common
+{
+    public class CogwormBossBar : ModBossBar
+    {
+        public override Asset<Texture2D> GetIconTexture(ref Rectangle? iconFrame)
+        {
+            return ModContent.Request<Texture2D>("Vanilla/Content/NPCs/CogwormHeadBoss");
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, NPC npc, ref BossBarDrawParams drawParams)
+        {
+            float lifePercent = drawParams.Life / drawParams.LifeMax;
+            float shakeIntensity = Utils.Clamp(1f - lifePercent - 0.2f, 0f, 1f);
+            drawParams.BarCenter.Y -= 20f;
+            drawParams.BarCenter += Main.rand.NextVector2Circular(0.5f, 0.5f) * shakeIntensity * 15f;
+
+            return true;
+        }
+    }
+}
---
+++ Common/TimerSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vanilla.Common
+{
+    public static class TimerSystem
+    {
+        private static List<DelayedAction> actions = new List<DelayedAction>();
+
+        public static void Update()
+        {
+            for (int i = actions.Count - 1; i >= 0; i--)
+            {
+                if (actions[i].Update())
+                {
+                    actions.RemoveAt(i);
+                }
+            }
+        }
+
+        public static void DelayAction(int delay, Action action)
+        {
+            actions.Add(new DelayedAction(delay, action));
+        }
+
+        private class DelayedAction
+        {
+            private int timer;
+            private Action action;
+
+            public DelayedAction(int delay, Action action)
+            {
+                timer = delay;
+                this.action = action;
+            }
+
+            public bool Update()
+            {
+                timer--;
+                if (timer <= 0)
+                {
+                    action.Invoke();
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
