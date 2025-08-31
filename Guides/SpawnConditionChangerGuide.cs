00dfd970d7c697068d5b34fdedcfdb4e0f021719
swampgodperseverance <kirillefsh@gmail.com>
2025-08-31 12:14:55 +0200

    Update and rename CacodemonSpawnCondition.cs to SpawnConditionChangerGuide.cs

--- Guides/SpawnConditionChangerGuide.cs
+++ Guides/SpawnConditionChangerGuide.cs
@@ -7,27 +7,34 @@ using System.Collections.Generic;
 
 namespace Synergia.Common.GlobalNPCs.AI
 {
-    public class CacodemonSpawnCondition : GlobalNPC
+    public class CacodemonSpawnCondition : GlobalNPC // we can use it to make progression changes
     {
         public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
         {
-            if (!ModLoader.TryGetMod("ValhallaMod", out Mod valhallaMod))
+            if (!ModLoader.TryGetMod("ValhallaMod", out Mod valhallaMod)) //mod
                 return;
 
-            if (!valhallaMod.TryFind("Cacodemon", out ModNPC cacodemon))
-                return;
+            if (!valhallaMod.TryFind("Cacodemon", out ModNPC cacodemon)) // NPC from this mod
+                return; // If it doesn't exist, do nothing
 
-            int cacodemonType = cacodemon.Type;
+            int cacodemonType = cacodemon.Type; //i think you understand
 
+            // Conditions for spawning Cacodemon:
+            //  Player is in the Underworld
+            //  Golem has been defeated
+            //  No invasion is currently active
+            //  Player is active (not dead, not disconnected)
             if (spawnInfo.Player.ZoneUnderworldHeight &&
                 NPC.downedGolemBoss &&
                 Main.invasionType == 0 &&         
                 spawnInfo.Player.active)            
             {
+            // Add Cacodemon to the spawn pool with 15% weight
                 pool[cacodemonType] = 0.15f;
             }
             else
             {
+             // Otherwise, ensure it cannot spawnn
                 pool.Remove(cacodemonType);
             }
 
@@ -37,7 +44,9 @@ namespace Synergia.Common.GlobalNPCs.AI
         {
             if (player.ZoneUnderworldHeight && NPC.downedGolemBoss)
             {
+            // Increase the maximum number of enemies that can exist at on
                 maxSpawns = (int)(maxSpawns * 1.5f);
+                // cooldown
                 spawnRate = (int)(spawnRate * 0.7f); 
             }
         }
