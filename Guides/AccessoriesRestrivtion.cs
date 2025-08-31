6334bb182cf83d3ecad7ebec4a3d4ed5d2ea7557
swampgodperseverance <kirillefsh@gmail.com>
2025-08-31 11:55:07 +0200

    Add files via upload

---
+++ Guides/AccessoriesRestrivtion.cs
@@ -0,0 +1,64 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Avalon.Items.Accessories.Hardmode;
+
+namespace Synergia.Common.GlobalItems
+{
+    public class SummonerAccessoryRestriction : GlobalItem
+    {
+        public override bool CanEquipAccessory(Item item, Player player, int slot, bool modded)
+        {
+
+            if (item.type == ItemID.SummonerEmblem)
+            {
+
+                for (int i = 3; i < 8 + player.extraAccessorySlots; i++) 
+                {
+                    Item equippedItem = player.armor[i];
+                    if (equippedItem != null && equippedItem.type == ModContent.ItemType<SummonerScroll>())
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            if (item.type == ModContent.ItemType<SummonerScroll>())
+            {
+                for (int i = 3; i < 8 + player.extraAccessorySlots; i++)
+                {
+                    Item equippedItem = player.armor[i];
+                    if (equippedItem != null && equippedItem.type == ItemID.SummonerEmblem)
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            if (item.type == ModContent.ItemType<SummonerScroll>())
+            {
+                for (int i = 3; i < 8 + player.extraAccessorySlots; i++)
+                {
+                    Item equippedItem = player.armor[i];
+                    if (equippedItem != null && equippedItem.type == ItemID.PapyrusScarab)
+                    {
+                        return false; 
+                    }
+                }
+            }
+            if (item.type == ItemID.PapyrusScarab)
+            {
+                for (int i = 3; i < 8 + player.extraAccessorySlots; i++)
+                {
+                    Item equippedItem = player.armor[i];
+                    if (equippedItem != null && equippedItem.type == ModContent.ItemType<SummonerScroll>())
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            return true; 
+        }
+    }
+}
