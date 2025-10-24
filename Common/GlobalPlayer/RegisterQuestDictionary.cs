0720c20d4f36eb6365f3214895d4420017e9db37
Sernk <135448749+Sernk@users.noreply.github.com>
2025-10-24 10:34:26 +0300

    Fix Quest

---
+++ Common/GlobalPlayer/RegisterQuestDictionary.cs
@@ -0,0 +1,25 @@
+﻿using Synergia.Common.ModSystems;
+using Synergia.Common.ModSystems.Hooks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ValhallaMod.NPCs.TownNPCs;
+using static Synergia.Common.QuestSystem.QuestConst;
+
+namespace Synergia.Common.GlobalPlayer
+{
+    public partial class QuestSystem
+    {
+        public class RegisterQuestDictionary : ModPlayer
+        {
+            public override void OnEnterWorld()
+            {
+                if (!HookForQuest.NpcQuestKeys.ContainsKey(ModContent.NPCType<Dwarf>())) {
+                    HookForQuest.NpcQuestKeys[ModContent.NPCType<Dwarf>()] = new QuestData(DwafPositionX, DWARF, 0, 1);
+                }
+                if (!HookForQuest.NpcQuestKeys.ContainsKey(NPCID.TaxCollector)) {
+                    HookForQuest.NpcQuestKeys[NPCID.TaxCollector] = new QuestData(BasePositionX, TAXC, 0, 1);
+                }
+            }
+        }
+    }
+}
