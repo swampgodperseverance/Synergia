1c6c196358d4011f5bdcb2b4fe28bc91726dd65a
swampgodperseverance <kirillefsh@gmail.com>
2025-10-27 21:41:28 +0100

    Update Synergia.cs

--- Synergia.cs
+++ Synergia.cs
@@ -115,6 +115,8 @@ namespace Synergia
         public static readonly SoundStyle SandSphere = new("Synergia/Assets/Sounds/SandSphere");
         public static readonly SoundStyle Impulse = new("Synergia/Assets/Sounds/Impulse");
         public static readonly SoundStyle Watersound = new("Synergia/Assets/Sounds/Watersound");
+        public static readonly SoundStyle Lasershot = new("Synergia/Assets/Sounds/Lasershot");
+        public static readonly SoundStyle Shotgun = new("Synergia/Assets/Sounds/Shotgun");
     }
 
     public class SynergiaSystems : ModSystem
