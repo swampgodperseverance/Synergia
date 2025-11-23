e0b5afadecc8216c1a28afdd0cdcce83179db3d8
swampgodperseverance <kirillefsh@gmail.com>
2025-11-23 19:33:07 +0100

    Add files via upload

--- Helpers/CorrectSwing.cs
+++ Helpers/CorrectSwing.cs
@@ -1,15 +1,1 @@
-﻿using Terraria.ModLoader;
-
-namespace Synergia.Helpers
-//Helper from lunar veil
-{
-    public class CorrectSwing : ModPlayer
-    {
-        public int SwingChange = 0;
-        public bool SwingChangeBool = false;
-
-
-
-
-    }
-}
+﻿
--- Helpers/MathFuncitons.cs
+++ Helpers/MathFuncitons.cs
@@ -9,5 +9,12 @@ namespace Synergia.Helpers
             double w = 2 * Math.PI * frequency;
             return amplitude * Math.Sin(w * x + phase);
         }
+         public static float EaseInBackForFlamingSword(float x)
+        {
+                double c1 = 1.70158;
+                float c3 = (float)c1 + 1;
+
+                return (float)(c3 * x * x * x - c1 * x * x);
+        }
     }
 }
