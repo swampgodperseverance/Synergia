2c50dc1ae7c011de782d9e663b88afb4b692ef30
swampgodperseverance <kirillefsh@gmail.com>
2025-12-02 18:01:43 +0100

    Add files via upload

---
+++ Common/GlobalItems/Weapons/WaterGunGI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Synergia.Content.Projectiles.Reworks.Reworks2; 
+
+namespace Synergia.Content.GlobalItems.Weapons
+{
+    public class WaterGunGI : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            if (entity.ModItem == null)
+                return false;
+
+            return string.Equals(entity.ModItem.Mod?.Name, "ValhallaMod", StringComparison.OrdinalIgnoreCase)
+                && string.Equals(entity.ModItem.Name, "WaterFighter", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source,
+            Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+
+            type = ModContent.ProjectileType<WaterGunBubble>();
+
+            float spread = 0.22f; 
+            velocity = velocity.RotatedBy(Main.rand.NextFloat(-spread, spread));
+
+
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+
+            return false;
+        }
+    }
+}
