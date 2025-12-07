69ebaf9a9c41d83e0e7925c0da4aef6902f2a323
swampgodperseverance <kirillefsh@gmail.com>
2025-12-07 13:00:59 +0100

    Add files via upload

---
+++ Common/GlobalItems/Weapons/SunstormGI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Synergia.Content.Projectiles.Reworks.Reworks2;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Synergia.Content.Projectiles.Reworks;
+
+namespace Synergia.Content.Global
+{
+    public class SunstormGI : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            if (entity.ModItem == null)
+                return false;
+
+            var modName = entity.ModItem.Mod?.Name;
+            var itemName = entity.ModItem?.Name;
+
+            return string.Equals(modName, "Avalon", StringComparison.OrdinalIgnoreCase)
+                && string.Equals(itemName, "Sunstorm", StringComparison.OrdinalIgnoreCase);
+        }
+
+
+        
+
+        public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source,
+            Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+           
+            Projectile.NewProjectile(
+                source,
+                position,
+                velocity,
+                ModContent.ProjectileType<SunRework>(), 
+                damage,
+                knockback,
+                player.whoAmI
+            );
+
+            return false;
+        }
+
+    }
+}
