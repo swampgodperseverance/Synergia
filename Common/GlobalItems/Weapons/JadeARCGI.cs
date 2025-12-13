0c808ea3c20fb1f3e553a89e247fd8b7037ba648
swampgodperseverance <kirillefsh@gmail.com>
2025-12-13 20:45:19 +0100

    Add files via upload

---
+++ Common/GlobalItems/Weapons/JadeARCGI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Synergia.Content.Projectiles.Reworks.Reworks2;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+namespace Synergia.Content.Global
+{
+    public class JadEARcanumGI : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            if (entity.ModItem == null)
+                return false;
+
+            var modName = entity.ModItem.Mod?.Name;
+            var itemName = entity.ModItem?.Name;
+
+            return string.Equals(modName, "ValhallaMod", StringComparison.OrdinalIgnoreCase)
+                && string.Equals(itemName, "JadeArcanum", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source,
+            Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+
+            Vector2 spawnPosition = Main.MouseWorld;
+
+
+            Projectile.NewProjectile(
+                source,
+                spawnPosition,
+                velocity, 
+                ModContent.ProjectileType<JadeGreatJavelin>(),
+                damage,
+                knockback,
+                player.whoAmI
+            );
+
+            return false; 
+        }
+    }
+}
---
+++ Common/GlobalItems/Weapons/JadeShortGI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Synergia.Content.Projectiles.Reworks.Reworks2;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+namespace Synergia.Content.Global
+{
+    public class JadEShortGI : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            if (entity.ModItem == null)
+                return false;
+
+            var modName = entity.ModItem.Mod?.Name;
+            var itemName = entity.ModItem?.Name;
+
+            return string.Equals(modName, "ValhallaMod", StringComparison.OrdinalIgnoreCase)
+                && string.Equals(itemName, "JadeShortsword", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source,
+            Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+           
+            Vector2 shootVel = velocity.SafeNormalize(Vector2.UnitX) * 12f;
+
+            Projectile.NewProjectile(
+                source,
+                position,
+                shootVel,
+                ModContent.ProjectileType<JadeShard>(),
+                (int)(damage * 0.8f), 
+                knockback,
+                player.whoAmI
+            );
+
+
+            return true;
+        }
+    }
+}
