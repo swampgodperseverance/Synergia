eb82eeda5058c2613f7a984bcf9ca73562de3ba0
swampgodperseverance <kirillefsh@gmail.com>
2025-12-14 19:58:18 +0100

    Add files via upload

---
+++ Content/Projectiles/Reworks/Reworks2/QueenBeeProj.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+using Synergia.Common.GlobalPlayer;
+
+namespace Synergia.Content.Projectiles.Reworks.Reworks2
+{
+    public class QueenBeeProj : ModProjectile
+    {
+        private const int TotalFrames = 4;
+        private int frameCounter = 0;
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = TotalFrames;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 168;
+            Projectile.height = 134;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.timeLeft = 600;
+            Projectile.alpha = 255;
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            SoundEngine.PlaySound(SoundID.Roar, Projectile.position);
+            Player player = Main.player[Projectile.owner];
+            player.GetModPlayer<ScreenShakePlayer>().TriggerShake(7, 1.1f);
+        }
+
+
+        public override void AI()
+        {
+            frameCounter++;
+            if (frameCounter >= 3)
+            {
+                frameCounter = 0;
+                Projectile.frame++;
+                if (Projectile.frame >= TotalFrames)
+                    Projectile.frame = 0;
+            }
+
+            if (Projectile.alpha > 0)
+            {
+                Projectile.alpha -= 10;
+                if (Projectile.alpha < 0)
+                    Projectile.alpha = 0;
+            }
+
+            float speed = 18f;
+            Vector2 target = new Vector2(-Projectile.width, Projectile.position.Y);
+            Vector2 direction = target - Projectile.position;
+            direction.Normalize();
+            Projectile.velocity = direction * speed;
+
+            if (Projectile.position.X < 100)
+            {
+                Projectile.alpha += 10;
+                if (Projectile.alpha > 255)
+                    Projectile.Kill();
+            }
+
+            Projectile.damage = 500;
+            Projectile.knockBack = 5f;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+            Rectangle frame = new Rectangle(0, Projectile.frame * texture.Height / TotalFrames, texture.Width, texture.Height / TotalFrames);
+            Vector2 origin = frame.Size() / 2f;
+            SpriteEffects effects = SpriteEffects.None;
+
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, frame, Color.White * ((255 - Projectile.alpha) / 255f), Projectile.rotation, origin, Projectile.scale, effects, 0f);
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/Reworks/Reworks2/SpaceCowboyRework.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Synergia.Trails;
+
+namespace Synergia.Content.Projectiles.Reworks.Reworks2
+{
+	// Token: 0x020000DF RID: 223
+	public class SpaceCowboyRework : ModProjectile
+	{
+		// Token: 0x06000483 RID: 1155 RVA: 0x0002AD01 File Offset: 0x00028F01
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.TrailCacheLength[base.Projectile.type] = 35;
+			ProjectileID.Sets.TrailingMode[base.Projectile.type] = 3;
+		}
+		private Vector2 startVelocity;
+		private bool velocityInitialized;
+		private int accelTimer;
+		private const int AccelDuration = 20;
+		// Token: 0x06000484 RID: 1156 RVA: 0x00031A18 File Offset: 0x0002FC18
+		public override bool PreDraw(ref Color lightColor)
+		{
+			default(SpaceCowboyShot).Draw(base.Projectile);
+			return true;
+		}
+
+		// Token: 0x06000485 RID: 1157 RVA: 0x00031A3C File Offset: 0x0002FC3C
+		public override void SetDefaults()
+		{
+			base.Projectile.width = 14;
+			base.Projectile.height = 14;
+			base.Projectile.aiStyle = 0;
+			base.Projectile.friendly = true;
+			base.Projectile.penetrate = 7;
+			base.Projectile.tileCollide = true;
+			base.Projectile.timeLeft = 700;
+			base.Projectile.DamageType = DamageClass.Magic;
+			base.Projectile.extraUpdates = 1;
+		}
+
+		// Token: 0x06000486 RID: 1158 RVA: 0x0002ADCF File Offset: 0x00028FCF
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+		
+		}
+
+			public override void AI()
+		{
+			if (!velocityInitialized)
+			{
+				startVelocity = Projectile.velocity;
+				velocityInitialized = true;
+			}
+
+			if (accelTimer < AccelDuration)
+			{
+				accelTimer++;
+
+				float t = accelTimer / (float)AccelDuration; 
+				float ease = Helpers.EaseFunctions.EaseInQuint(t);
+
+				float speedMultiplier = MathHelper.Lerp(0.4f, 2.2f, ease);
+				Projectile.velocity = startVelocity * speedMultiplier;
+			}
+
+			Projectile.rotation =
+				(float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X)
+				+ MathHelper.ToRadians(90f);
+
+			int dustIndex = Dust.NewDust(
+				Projectile.position,
+				Projectile.width,
+				Projectile.height,
+				174,
+				0f,
+				0f,
+				Projectile.alpha,
+				default,
+				0.5f
+			);
+
+			Dust dust = Main.dust[dustIndex];
+			dust.noGravity = true;
+			dust.color = new Color(0, 220, 255);
+		}
+
+
+
+		public override Color? GetAlpha(Color lightColor)
+		{
+			return new Color?(Color.White);
+		}
+
+		public override void OnKill(int timeLeft)
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				Dust.NewDust(base.Projectile.position, base.Projectile.width, base.Projectile.height, 174, 0f, 0f, base.Projectile.alpha, default(Color), 1f);
+			}
+		}
+	}
+}
