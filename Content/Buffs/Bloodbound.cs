8a8ff17a75d83ece60b96d513a4ea4bd25ba63a6
swampgodperseverance <kirillefsh@gmail.com>
2025-12-19 16:09:18 +0100

    Add files via upload

---
+++ Content/Buffs/Bloodbound.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Synergia.Content.Buffs
+{
+    public class Bloodbound : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+
+            
+
+            Main.debuff[Type] = false;
+            Main.buffNoSave[Type] = false;
+            Main.buffNoTimeDisplay[Type] = true;    
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+       
+            player.GetAttackSpeed(DamageClass.Throwing) += 0.1f;
+      
+            player.moveSpeed += 0.2f;
+
+            player.endurance -= 0.10f;
+
+            if (Main.rand.NextBool(6)) 
+            {
+                Dust dust = Dust.NewDustDirect(
+                    player.position,
+                    player.width,
+                    player.height,
+                    DustID.Blood,
+                    Main.rand.NextFloat(-1f, 1f),
+                    Main.rand.NextFloat(0.5f, 2f),
+                    100,
+                    default,
+                    Main.rand.NextFloat(0.8f, 1.2f)
+                );
+
+                dust.noGravity = false;
+                dust.velocity *= 0.5f;
+            }
+        }
+    }
+}
