0163fb719f68e9ad7f8e98e0767320198e00fc13
swampgodperseverance <kirillefsh@gmail.com>
2025-12-20 20:18:38 +0100

    Add files via upload

---
+++ Content/Projectiles/Thrower/HeavyStone.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using NewHorizons.Content.Projectiles.Throwing;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Synergia.Content.Projectiles.Thrower
+{
+	public class HeavyStone : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 3;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 22;
+			Projectile.height = 22;
+
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+
+			Projectile.penetrate = 1;
+			Projectile.timeLeft = 240;
+
+			Projectile.tileCollide = true;
+			Projectile.ignoreWater = false;
+
+			Projectile.DamageType = DamageClass.Throwing;
+		}
+
+		public override void AI()
+		{
+			Projectile.velocity.Y += 0.6f;
+			if (Projectile.velocity.Y > 16f)
+				Projectile.velocity.Y = 16f;
+
+			Projectile.rotation += Projectile.velocity.X * 0.08f;
+
+			if (Projectile.velocity.Y == 0f)
+			{
+				Projectile.velocity.X *= 0.97f; 
+				if (Math.Abs(Projectile.velocity.X) < 0.1f)
+					Projectile.velocity.X = 0f;
+			}
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			
+			if (oldVelocity.Y > 2f)
+				Projectile.velocity.Y = -oldVelocity.Y * 0.25f;
+
+			if (Math.Abs(oldVelocity.X) > 1f)
+				Projectile.velocity.X = oldVelocity.X * 0.6f;
+
+			return false; 
+		}
+
+		public override void OnKill(int timeLeft)
+		{
+	
+			SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
+
+		
+			for (int i = 0; i < 25; i++)
+			{
+				Dust.NewDust(
+					Projectile.position,
+					Projectile.width,
+					Projectile.height,
+					DustID.Stone,
+					Main.rand.NextFloat(-4f, 4f),
+					Main.rand.NextFloat(-4f, 4f)
+				);
+			}
+
+
+			int count = Main.rand.Next(6, 10);
+			for (int i = 0; i < count; i++)
+			{
+				Vector2 velocity = Main.rand.NextVector2Circular(6f, 6f);
+
+				Projectile.NewProjectile(
+					Projectile.GetSource_Death(),
+					Projectile.Center,
+					velocity,
+					ModContent.ProjectileType<RockProj>(),
+					Projectile.damage / 2,
+					1f,
+					Projectile.owner
+				);
+			}
+		}
+	}
+}
---
+++ Content/Projectiles/Thrower/TeethBreakerMega.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework.Graphics;
+using ValhallaMod;
+using Terraria.ModLoader;
+using ValhallaMod.Projectiles.AI;
+
+namespace Synergia.Content.Projectiles.Thrower
+{
+	// Token: 0x020001F4 RID: 500
+	public class TeethBreakerMega : ValhallaGlaive
+	{
+		// Token: 0x06000955 RID: 2389 RVA: 0x00012DA5 File Offset: 0x00010FA5
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.TrailCacheLength[base.Projectile.type] = 4;
+			ProjectileID.Sets.TrailingMode[base.Projectile.type] = 0;
+		}
+
+		// Token: 0x06000956 RID: 2390 RVA: 0x0005B780 File Offset: 0x00059980
+		public override void SetDefaults()
+		{
+			base.Projectile.width = 22;
+			base.Projectile.height = 22;
+			base.Projectile.friendly = true;
+			base.Projectile.DamageType = DamageClass.Throwing;
+			base.Projectile.alpha = 0;
+			base.Projectile.penetrate = 103;
+			base.Projectile.scale = 1f;
+			base.Projectile.tileCollide = true;
+			base.Projectile.extraUpdates = 1;
+			this.glaive = true;
+			this.bounces = 0;
+			this.timeFlying = 30;
+			this.speedHoming = 9f;
+			this.speedFlying = 10f;
+			this.speedComingBack = 24f;
+			this.homingDistanceMax = 200f;
+			this.homingStyle = 2;
+			this.homingStart = false;
+			this.tileBounce = true;
+			this.rotationSpeed = 0.20f;
+		}
+
+		// Token: 0x06000957 RID: 2391 RVA: 0x0003DD39 File Offset: 0x0003BF39
+		public override bool PreDraw(ref Color lightColor)
+		{
+			return DrawHelper.DrawTrail(base.Projectile, Main.spriteBatch, lightColor * 0.25f, 2, true, false);
+		}
+	    public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			Point tilePos = Projectile.Center.ToTileCoordinates();
+
+			for (int x = -1; x <= 1; x++)
+			{
+				for (int y = -1; y <= 1; y++)
+				{
+					int i = tilePos.X + x;
+					int j = tilePos.Y + y;
+
+					if (WorldGen.InWorld(i, j))
+					{
+						Tile tile = Main.tile[i, j];
+						if (tile.HasTile && Main.tileSolid[tile.TileType])
+						{
+							WorldGen.KillTile(i, j, false, false, true);
+						}
+					}
+				}
+			}
+
+			return false; // чтобы глефа НЕ уничтожалась
+		}
+
+	}
+}
