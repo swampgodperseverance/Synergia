520cae4b9b5b52734a5f46c0c58e99ac9981af64
swampgodperseverance <kirillefsh@gmail.com>
2026-02-05 18:53:21 +0100

    Add files via upload

--- Content/Items/Weapons/Ranged/Enfer.cs
+++ Content/Items/Weapons/Ranged/Enfer.cs
@@ -4,6 +4,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.ID;
+using Microsoft.Xna.Framework;
 
 namespace Synergia.Content.Items.Weapons.Ranged
 {
@@ -13,7 +14,6 @@ namespace Synergia.Content.Items.Weapons.Ranged
 
         public override void SetStaticDefaults()
         {
-
         }
 
         public override void SetDefaults()
@@ -22,8 +22,8 @@ namespace Synergia.Content.Items.Weapons.Ranged
             Item.DamageType = DamageClass.Ranged;
             Item.width = 32;
             Item.height = 64;
-            Item.useTime = 30;
-            Item.useAnimation = 30;
+            Item.useTime = 45;
+            Item.useAnimation = 45;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 2.5f;
@@ -39,10 +39,12 @@ namespace Synergia.Content.Items.Weapons.Ranged
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source,
                                    Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            int numberProjectiles = 2;
+            int numberProjectiles = Main.rand.Next(2, 5);
+
             for (int i = 0; i < numberProjectiles; i++)
             {
                 Vector2 perturbedSpeed = velocity.RotatedByRandom(MathHelper.ToRadians(5));
+
                 Projectile.NewProjectile(
                     source,
                     position,
@@ -53,24 +55,27 @@ namespace Synergia.Content.Items.Weapons.Ranged
                     player.whoAmI
                 );
             }
+
             return false;
         }
 
         public override Vector2? HoldoutOffset() => new Vector2(-4f, 0f);
+
         public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame,
             Color drawColor, Color itemColor, Vector2 origin, float scale)
         {
             Texture2D texture = TextureAssets.Item[Item.type].Value;
             float pulse = (float)(Math.Sin(Main.GlobalTimeWrappedHourly * 4f) * 0.2f + 0.8f);
             spriteBatch.Draw(texture, position, frame, drawColor, 0f, origin, scale, SpriteEffects.None, 0f);
+
             if (glowTexture != null)
             {
                 Color glowColor = new Color(255, 120, 40, 255) * (0.5f + pulse * 0.5f);
                 spriteBatch.Draw(glowTexture, position, frame, glowColor, 0f, origin, scale, SpriteEffects.None, 0f);
             }
-
             return false;
         }
+
         public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor,
             ref float rotation, ref float scale, int whoAmI)
         {
@@ -78,12 +83,13 @@ namespace Synergia.Content.Items.Weapons.Ranged
             Vector2 position = Item.position - Main.screenPosition + new Vector2(Item.width / 2f, Item.height - texture.Height / 2f);
             Rectangle? frame = texture.Frame();
             Vector2 origin = frame.Value.Size() / 2f;
+
             spriteBatch.Draw(texture, position, frame, lightColor, rotation, origin, scale, SpriteEffects.None, 0f);
+
             if (glowTexture != null)
             {
                 float pulse = (float)(Math.Sin(Main.GlobalTimeWrappedHourly * 5f) * 0.2f + 0.8f);
                 Color glowColor = new Color(255, 100, 20) * (0.6f + pulse * 0.4f);
-
                 spriteBatch.Draw(glowTexture, position, frame, glowColor, rotation, origin, scale * 1.05f, SpriteEffects.None, 0f);
             }
 
@@ -93,8 +99,9 @@ namespace Synergia.Content.Items.Weapons.Ranged
                 Main.dust[dust].velocity *= 0.3f;
                 Main.dust[dust].noGravity = true;
             }
-
             return false;
         }
+
+
     }
 }
