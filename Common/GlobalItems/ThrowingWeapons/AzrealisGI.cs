25817f577f3a8d5920dc07f2af123e660a0d17a5
swampgodperseverance <kirillefsh@gmail.com>
2026-02-08 13:48:30 +0100

    Add files via upload

---
+++ Common/GlobalItems/ThrowingWeapons/AzrealisGI.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Synergia.Common.GlobalItems.ThrowingWeapons;
+using Terraria;
+using Terraria.DataStructures;
+using Synergia.Content.Projectiles.Thrower;
+using Terraria.ID;
+using Bismuth.Content.Items.Weapons.Throwing;
+using ValhallaMod.Items.Weapons.Blood;
+
+namespace Synergia.Common.GlobalItems
+{
+    public class aZREALISGlobalItem : ThrowingGI {
+        public override int ItemType => ModContent.ItemType<AzraelsHeartstopper>(); 
+        public override bool NewBehavior(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            int newType = ModContent.ProjectileType<AzraelsHeartstopper2>();
+            velocity *= 1.3f;
+            damage = (int)(damage * 1.1f);
+            Projectile.NewProjectile(source, position, velocity, newType, damage, knockback, player.whoAmI);
+
+            return false;
+        }
+    }
+}
---
+++ Common/GlobalItems/ThrowingWeapons/ShroomerangGI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Synergia.Common.GlobalItems.ThrowingWeapons;
+using Synergia.Content.Projectiles.Thrower;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace Synergia.Common.GlobalItems
+{
+    public class ShroomeranglGlobalItem : ThrowingGI {
+        public override int ItemType => ItemID.Shroomerang;
+        public override bool NewBehavior(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            int newType = ModContent.ProjectileType<Shroomerang2>();
+            velocity *= 1.2f;
+            damage = (int)(damage * 1f);
+            knockback *= 1.2f;
+            Projectile.NewProjectile(source, position, velocity, newType, damage, knockback, player.whoAmI);
+            return false;
+        }
+    }
+}
