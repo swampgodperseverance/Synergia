f04799e19919e43968f33a19e8d4345bb38cca6a
swampgodperseverance <kirillefsh@gmail.com>
2026-02-19 23:49:56 +0100

    Add files via upload

--- Content/Projectiles/Friendly/AirflowProjectile.cs
+++ Content/Projectiles/Friendly/AirflowProjectile.cs
@@ -2,62 +2,40 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
-using Synergia.Content.Projectiles.Friendly;
 
 namespace Synergia.Content.Projectiles.Friendly
 {
-	public class AirflowProjectile : ModProjectile
-	{
-		private const float FeatherSpawnRate = 40 * 2;
-		private int featherTimer = 0;
+    public class AirflowProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            var proj = Projectile;
 
-		public override void SetDefaults()
-		{
-			var proj = Projectile;
+            proj.width = 20;
+            proj.height = 20;
+            proj.aiStyle = 3;
+            proj.friendly = true;
+            proj.DamageType = DamageClass.Throwing;
+            proj.penetrate = -1;
+            proj.timeLeft = 60 * 5;
+        }
 
-			proj.width = 20;
-			proj.height = 20;
-			proj.aiStyle = 3;
-			proj.friendly = true;
-			proj.DamageType = DamageClass.Throwing;
-			proj.penetrate = -1;
-			proj.timeLeft = 60 * 5;
-		}
-
-		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-		{
-			for (int i = 0; i < 5; i++)
-			{
-				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height,
-				DustID.BlueCrystalShard, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100, default, 1.2f);
-			}
-		}
-
-		public override void AI()
-		{
-			featherTimer++;
-			
-			if (featherTimer >= FeatherSpawnRate)
-			{
-				featherTimer = 0;
-				SpawnHarpyFeathers();
-			}
-		}
-
-		private void SpawnHarpyFeathers()
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				Projectile.NewProjectile(
-					Projectile.GetSource_FromThis(),
-					Projectile.Center,
-					new Vector2(Main.rand.NextFloat(-3f, 3f), Main.rand.NextFloat(-5f, 0f)),
-					ModContent.ProjectileType<AirflowFeather>(),
-					20, 
-					Projectile.knockBack * 0.5f,
-					Projectile.owner
-				);
-			}
-		}
-	}
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Dust.NewDust(
+                    Projectile.position,
+                    Projectile.width,
+                    Projectile.height,
+                    DustID.BlueCrystalShard,
+                    Projectile.velocity.X * 0.2f,
+                    Projectile.velocity.Y * 0.2f,
+                    100,
+                    default,
+                    1.2f
+                );
+            }
+        }
+    }
 }
