6ab4d0ae3afb0674cf21084311d10e33b8f079bb
Aeris <135448749+Sernk@users.noreply.github.com>
2026-02-26 23:57:35 +0200

    Update EditSound.cs

--- Common/EditSound.cs
+++ Common/EditSound.cs
@@ -4,18 +4,16 @@ using Synergia.Content.Buffs;
 using Terraria;
 
 namespace Synergia.Common {
-    public partial class EditSound {
-        public class PeacefulTown : ModSceneEffect {
-            string song;
-            public override int Music => MusicLoader.GetMusicSlot(GetSongByName2(song));
-            public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
-            public override bool IsSceneEffectActive(Player player) {
-                bool resolute = false;
-                if (player == null || !player.active) { return false; }
-                if (player.HasBuff<AuraOfEmpire>() || player.HasBuff<SnowVillageBuff>()) { song = "PeacefulTownV2"; resolute = true; }
-                if (player.InModBiome<NewHell>()) { song = "InfernoFrontierSoundtrack"; resolute = true; }
-                return resolute;
-            }
+    public class EditSound : ModSceneEffect {
+        string song;
+        public override int Music => MusicLoader.GetMusicSlot(GetSongByName2(song));
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player) {
+            bool resolute = false;
+            if (player == null || !player.active) { return false; }
+            if (player.HasBuff<AuraOfEmpire>() || player.HasBuff<SnowVillageBuff>()) { song = "PeacefulTownV2"; resolute = true; }
+            if (player.InModBiome<NewHell>()) { song = "InfernoFrontierSoundtrack"; resolute = true; }
+            return resolute;
         }
     }
 }
