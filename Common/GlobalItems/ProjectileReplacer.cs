af1ccc9528a51b8e864e08dfb781cb327fd4e811
swampgodperseverance <kirillefsh@gmail.com>
2026-03-01 20:06:06 +0100

    Update ProjectileReplacer.cs

--- Common/GlobalItems/ProjectileReplacer.cs
+++ Common/GlobalItems/ProjectileReplacer.cs
@@ -26,7 +26,7 @@ namespace Synergia.Common.GlobalItems
                             item.shoot = ModContent.ProjectileType<Content.Projectiles.Reworks.TroxiniumRework>();
                             break;
                         case "NaquadahLance":
-                            item.shoot = ModContent.ProjectileType<Content.Projectiles.Reworks.NaquadahRework2>();
+                            item.shoot = ModContent.ProjectileType<Content.Projectiles.Reworks.NaquadahRework>();
                             break;
                         case "CaesiumPike":
                             item.shoot = ModContent.ProjectileType<Content.Projectiles.Reworks.CaesiumSpear>();
