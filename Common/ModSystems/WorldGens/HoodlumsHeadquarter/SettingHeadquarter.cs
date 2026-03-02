54cabc8536d0834e8ad7939733f183e53d3b5b0c
Aeris <135448749+Sernk@users.noreply.github.com>
2026-03-02 09:53:28 +0200

    1

--- Common/ModSystems/WorldGens/HoodlumsHeadquarter/SettingHeadquarter.cs
+++ Common/ModSystems/WorldGens/HoodlumsHeadquarter/SettingHeadquarter.cs
@@ -9,7 +9,7 @@ namespace Synergia.Common.ModSystems.WorldGens.HoodlumsHeadquarter {
         public override string NameGen => "[Synergia] Setting struct in swamp";
         public override int Index => 1;
         public override bool GensBool { get => swampGen; set => swampGen = value; }
-        public override bool Do_MakeGen(GenerationProgress progress) => HeadquarterLayerOne.GenHeadquarter(progress); // BismuthWorld.WorldSize == 1 ? HeadquarterLayerOne.GenHeadquarter(progress) : SwampCave.GenCave(progress);
+        public override bool Do_MakeGen(GenerationProgress progress) => HeadquarterLayerOne.GenHeadquarter(progress); // BismuthWorld.WorldSize != 1 ? HeadquarterLayerOne.GenHeadquarter(progress) : SwampCave.GenCave(progress);
         public override void PostWorldGen() {
             //HeadquarterLayerOne.SpawnLava();
         }
