32a0a4a2470ed23c93ad2189e7d5d71d424628d1
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-03-06 12:32:25 +0800

    Fixed UnholyTridentProj

--- Common/GlobalProjectiles/UnholyTridentProj.cs
+++ Common/GlobalProjectiles/UnholyTridentProj.cs
@@ -6,7 +6,7 @@ namespace Synergia.Common.GlobalProjectiles
 {
     public class UnholyTridentProj : GlobalProjectile
     {
-        public override bool AppliesToEntity(Projectile projectile, bool lateInstatiation) => projectile.type == ProjectileID.UnholyTridentFriendly
+        public override bool AppliesToEntity(Projectile projectile, bool lateInstatiation) => projectile.type == ProjectileID.UnholyTridentFriendly;
         public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone) => target.AddBuff(BuffID.ShadowFlame, 360, false);
     }
 }
