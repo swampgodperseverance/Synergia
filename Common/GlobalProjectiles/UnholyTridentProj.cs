206cc8bad41a9d129c0386ce397f9325c163cdee
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-03-06 12:30:37 +0800

    Fixed UnholyTridentProj

--- Common/GlobalProjectiles/UnholyTridentProj.cs
+++ Common/GlobalProjectiles/UnholyTridentProj.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria.ModLoader;
 using Terraria;
 using Terraria.ID;
 
@@ -10,10 +6,7 @@ namespace Synergia.Common.GlobalProjectiles
 {
     public class UnholyTridentProj : GlobalProjectile
     {
-        public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            if (projectile.type == ProjectileID.UnholyTridentFriendly)
-                target.AddBuff(BuffID.ShadowFlame, 360, false);
-        }
+        public override bool AppliesToEntity(Projectile projectile, bool lateInstatiation) => projectile.type == ProjectileID.UnholyTridentFriendly
+        public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone) => target.AddBuff(BuffID.ShadowFlame, 360, false);
     }
 }
