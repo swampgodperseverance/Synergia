7d3dc231936d90010eb73a8ffb822331efcbe8a5
swampgodperseverance <kirillefsh@gmail.com>
2026-03-06 11:13:28 +0100

    Horizons Compat

---
+++ Common/ModSystems/RecipeSystem/ChangesRecipe/HorizonsChanges/HorizonsWeapons.cs
@@ -0,0 +1,32 @@
+﻿using Avalon.Items.Accessories.Hardmode;
+using Avalon.Items.Accessories.PreHardmode;
+using Avalon.Items.Material.Ores;
+using Avalon.Items.Material.Shards;
+using Avalon.Items.Material.TomeMats;
+using Avalon.Items.Potions.Buff;
+using Avalon.Items.Tomes.PreHardmode;
+using Consolaria.Content.Items.Materials;
+using NewHorizons.Content.Items.Materials;
+using Starforgedclassic.Content.Accessories.SkyShield;
+using Terraria;
+using Terraria.ID;
+using ValhallaMod.Items.Accessory;
+using ValhallaMod.Items.Accessory.Shield;
+using ValhallaMod.Items.Garden;
+using ValhallaMod.Items.Material;
+using ValhallaMod.Items.Material.Bar;
+using static Terraria.ModLoader.ModContent;
+
+namespace Synergia.Common.ModSystems.RecipeSystem.ChangesRecipe.HorizonsChanges
+{
+        public class HorizonsWeapons : BaseRecipe {
+
+            public override void Ingredient(Recipe recipe) {
+
+
+
+        }
+
+    }
+    
+}
