088549313ce94269ef7980b5f474798573e0666c
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-03-06 12:08:00 +0800

    Fixed QuestDrawSystem

--- Common/GlobalNPCs/QuestDrawSystem.cs
+++ Common/GlobalNPCs/QuestDrawSystem.cs
@@ -1,4 +1,4 @@
-﻿using Avalon.NPCs.TownNPCs;
+using Avalon.NPCs.TownNPCs;
 using Bismuth.Utilities.ModSupport;
 using NewHorizons.Content.NPCs.Town;
 using Synergia.Content.NPCs;
@@ -11,18 +11,15 @@ using static Synergia.ModList;
 namespace Synergia.Common {
     public partial class QuestSystem {
         public class QuestDrawSystem : GlobalNPC {
-            /// <summary> обязательно нужно указать npcType </summary>
-            /// <param name="npcType"> Кому рисовать </param>
-            /// <param name="name">Ключ квеста</param>
+            public override bool AppliesToEntity(NPC npc, bool lateInstatiation) => npc.type == NPCType<Dwarf>() || npc.type == NPCID.TaxCollector || npc.type == Roa.Find<ModNPC>("Hunter").Type || npc.type == NPCType<Artist>() || npc.type == NPCType<Ninja>() || npc.type == NPCType<Librarian>() || npc.type == NPCType<HellDwarf>();
             public static void DrawQuestIcon(NPC npc, SpriteBatch spriteBatch, int npcType, string name) {
+                if (npc.type != npcType) return;
                 Player player = Main.LocalPlayer;
                 IEnumerable<IQuest> quests = QuestRegistry.GetAvailableQuests(player, name);
                 foreach (IQuest quest in quests) {
-                    if (npc.type == npcType) {
-                        bool showAvailable = quest.IsAvailable(player) && !quest.IsActive(player);
-                        bool showActive = quest.IsActive(player);
-                        quest?.IsActiveQuestUIIcon(showAvailable, showActive, spriteBatch, npc, player);
-                    }
+                    bool showAvailable = quest.IsAvailable(player) && !quest.IsActive(player);
+                    bool showActive = quest.IsActive(player);
+                    quest?.IsActiveQuestUIIcon(showAvailable, showActive, spriteBatch, npc, player);
                 }
             }
             public override void PostDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
