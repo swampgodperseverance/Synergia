60a2091c0dafa0b79d6fc09323454ac44df35062
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-03-08 17:59:15 +0800

    TRAE Boss Reworks removed from dependencies

--- ModList.cs
+++ ModList.cs
@@ -14,7 +14,6 @@ public static class ModList {
     public static Mod StarforgedClassic { get; private set; }
     public static Mod Valhalla { get; private set; }
     public static Mod TRAEProjectLoaded { get; private set; }
-    public static Mod TRAEProjectRework { get; private set; }
     public static Mod PackBuilderLoaded { get; private set; }
 
     public static void LoadMod() {
@@ -25,7 +24,6 @@ public static class ModList {
         Roa = ModLoader.GetMod("RoA");
         StarforgedClassic = ModLoader.GetMod("starforgedclassic");
         Valhalla = ModLoader.GetMod("ValhallaMod");
-        TRAEProjectRework = ModLoader.GetMod("TRAEBossRework");
         ModLoader.TryGetMod("TRAEProject", out Mod TRAEProject);
         ModLoader.TryGetMod("PackBuilder", out Mod PackBuilder);
         if (TRAEProject != null) { TRAEProjectLoaded = TRAEProject; }
