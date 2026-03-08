aaa9c36300f838790146e95037120dca50296ca5
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-03-08 18:08:02 +0800

    TRAE Boss Reworks removed from dependencies

--- Common/GlobalItems/Set/NewLootInBags.cs
+++ Common/GlobalItems/Set/NewLootInBags.cs
@@ -5,7 +5,6 @@ using Synergia.Content.Items.Weapons.Throwing;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using TRAEBossRework.NewContent.Items.DreadItems.BossBag;
 using ValhallaMod.Items.Consumable.Bag;
 using ValhallaMod.Items.Weapons.Ranged.Javelins;
 using static Synergia.Helpers.ItemHelper;
@@ -17,7 +16,7 @@ namespace Synergia.Common.GlobalItems.Set {
             AddLoot(item, itemLoot, ItemType<PirateSquidBag>(), ItemType<GoldGlove>(), 3);
             AddLoot(item, itemLoot, ItemID.GolemBossBag, ItemType<CaesiumChunk>(), 1, 60, 110);
             AddLoot(item, itemLoot, ItemType<LepusBag>(), ItemType<CarrotDagger>(), 2, 150, 150);
-            AddLoot(item, itemLoot, ItemType<DreadBag>(), ItemType<BloodyNecklace>(), 2);
         }
+        public override bool AppliesToEntity(Item item, bool lateInstantiation) => item.type == ItemType<PirateSquidBag>() || item.type == ItemID.GolemBossBag || item.type == ItemType<LepusBag>();
     }
 }
