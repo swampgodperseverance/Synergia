efdd7147c5a60b0064dfed12596155079fc24e20
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-03-29 04:53:11 +0800

    Fixed BacteriumPrime killing itself with poison

---
+++ Content/Projectiles/Hostile/PoisonGasTrapHostile.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Synergia.Content.Projectiles.Hostile
+{
+    public class PoisonGasTrap : Avalon.Projectiles.Hostile.TuhrtlOutpost.PoisonGasTrap
+    {
+        public override string Texture => "Avalon/Projectiles/Hostile/TuhrtlOutpost/PoisonGasTrap";
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.ForcePlateDetection[base.Projectile.type] = new bool?(false);
+            ProjectileID.Sets.NoLiquidDistortion[base.Type] = true;
+        }
+        public override bool? CanHitNPC(NPC target)
+        {
+            return false;
+        }
+    }
+}
