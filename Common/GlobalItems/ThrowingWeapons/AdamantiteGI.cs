932afd93c51fae2c03349ed003536d4d52776183
Aeris <135448749+Sernk@users.noreply.github.com>
2026-04-13 18:12:06 +0300

    .

--- Common/GlobalItems/ThrowingWeapons/AdamantiteGI.cs
+++ Common/GlobalItems/ThrowingWeapons/AdamantiteGI.cs
@@ -8,6 +8,7 @@ using NewHorizons.Content.Items.Weapons.Throwing;
 namespace Synergia.Common.GlobalItems
 {
     public class ADAMANTITEGlobalItem : ThrowingGI {
+        public override string AbilityInfo => "AdamantDaggerInfo";
         public override int ItemType => ModContent.ItemType<AdamantiteDagger>(); 
         public override bool NewBehavior(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
             int newType = ModContent.ProjectileType<AdamantiteDagger2>();
--- Common/GlobalItems/ThrowingWeapons/ThrowingGI.cs
+++ Common/GlobalItems/ThrowingWeapons/ThrowingGI.cs
@@ -1,7 +1,7 @@
 ﻿using Synergia.Common.GlobalPlayer;
+using Synergia.Common.Rarities;
 using System.Collections.Generic;
 using Terraria;
-using static Synergia.Common.SUtils.LocUtil;
 
 namespace Synergia.Common.GlobalItems.ThrowingWeapons {
     public abstract class ThrowingGI : GlobalItem {
@@ -10,21 +10,17 @@ namespace Synergia.Common.GlobalItems.ThrowingWeapons {
         public virtual string AbilityInfo => "BaseText";
         public sealed override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemType;
         public sealed override bool Shoot(Item item, Player player, Terraria.DataStructures.EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-            ThrowingPlayer modPlayer = player.GetModPlayer<ThrowingPlayer>();
-            if (modPlayer.DoubleMode) {
-                return NewBehavior(item, player, source, position, velocity, type, damage, knockback);
-            }
+            if (player.GetModPlayer<ThrowingPlayer>().DoubleMode) { return NewBehavior(item, player, source, position, velocity, type, damage, knockback); }
             return true;
         }
         public sealed override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone) {
-            ThrowingPlayer throwing = player.GetModPlayer<ThrowingPlayer>();
-            if (throwing.comboCount <= 5) {
-                throwing.AddCombo();
-            }
+            if (player.GetModPlayer<ThrowingPlayer>().comboCount <= 5) { player.GetModPlayer<ThrowingPlayer>().AddCombo(); }
         }
         public sealed override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
-            tooltips.Add(new TooltipLine(Mod, "ThrowingItem", ItemTooltip(WEP, "Info")));
-            tooltips.Add(new TooltipLine(Mod, "Ability", ItemTooltip(WEP, AbilityInfo)));
+            Color color = BaseRarity.AnimatedColor([Color.Gray, Color.LightGray, Color.White], 45);
+            tooltips.Add(new TooltipLine(Mod, "ThrowingItem", ItemTooltip(WEP, "Info")) { OverrideColor = color });
+            tooltips.Add(new TooltipLine(Mod, "Ability", ItemTooltip(WEP, "BaseText")) { OverrideColor = color });
+            tooltips.Add(new TooltipLine(Mod, "ActiveAbility", ItemTooltip(WEP, AbilityInfo)) { OverrideColor = color });
         }
     }
 }
