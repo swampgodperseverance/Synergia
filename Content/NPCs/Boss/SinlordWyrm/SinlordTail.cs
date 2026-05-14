4ad854c125c0380401183329292e4a2ec60dc660
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2026-05-14 13:31:15 +0800

    Updated Sinlord SFX

--- Content/NPCs/Boss/SinlordWyrm/SinlordTail.cs
+++ Content/NPCs/Boss/SinlordWyrm/SinlordTail.cs
@@ -19,21 +19,15 @@ namespace Synergia.Content.NPCs.Boss.SinlordWyrm
 			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
 			NPCID.Sets.ImmuneToRegularBuffs[Type] = true;
 		}
-        public override void HitEffect(NPC.HitInfo hit)
-        {
-            if (NPC.life <= 0)
-            {
-                if (!Main.dedServ)
-                {
-                    var source = NPC.GetSource_Death();
-
-                    Gore.NewGore(source, NPC.position, NPC.velocity, Mod.Find<ModGore>("SinlordGore7").Type);
-                    Gore.NewGore(source, NPC.position, NPC.velocity, Mod.Find<ModGore>("SinlordGore8").Type);
-                    Gore.NewGore(source, NPC.position, NPC.velocity, Mod.Find<ModGore>("SinlordGore9").Type);
-                }
-            }
-        }
-        public override bool PreDraw(SpriteBatch sprite, Vector2 screenPosition, Color lightColor) {
+		public override void HitEffect(NPC.HitInfo hit)  {
+			if(NPC.life <= 0 && !Main.dedServ) {
+				var source = NPC.GetSource_Death();
+				Gore.NewGore(source, NPC.position, NPC.velocity, Mod.Find<ModGore>("SinlordGore7").Type);
+				Gore.NewGore(source, NPC.position, NPC.velocity, Mod.Find<ModGore>("SinlordGore8").Type);
+				Gore.NewGore(source, NPC.position, NPC.velocity, Mod.Find<ModGore>("SinlordGore9").Type);
+			}
+		}
+		public override bool PreDraw(SpriteBatch sprite, Vector2 screenPosition, Color lightColor) {
 			lightColor = NPC.GetNPCColorTintedByBuffs(lightColor);
 			Texture2D texture = (Texture2D)ModContent.Request<Texture2D>(Texture);
 			sprite.Draw(texture, NPC.Center - screenPosition, null, lightColor, NPC.rotation + MathHelper.PiOver2, new Vector2(texture.Width) * 0.5f, NPC.scale, SpriteEffects.None, 0);
