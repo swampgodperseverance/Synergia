82c28514faa285429ac26304519c73b6ff6f22cf
Aeris <135448749+Sernk@users.noreply.github.com>
2026-06-11 15:11:25 +0300

    Update SettingHeadquarter.cs

--- Common/ModSystems/WorldGens/HoodlumsHeadquarter/SettingHeadquarter.cs
+++ Common/ModSystems/WorldGens/HoodlumsHeadquarter/SettingHeadquarter.cs
@@ -14,9 +14,7 @@ namespace Synergia.Common.ModSystems.WorldGens.HoodlumsHeadquarter {
         public override int Index => 1;
         public override bool GensBool { get => swampGen; set => swampGen = value; }
         public override bool Do_MakeGen(GenerationProgress progress) => HeadquarterLayerOne.GenHeadquarter(progress); // BismuthWorld.WorldSize != 1 ? HeadquarterLayerOne.GenHeadquarter(progress) : SwampCave.GenCave(progress);
-        public override void PostWorldGen() {
-            //HeadquarterLayerOne.SpawnLava();
-        }
+        public override void PostWorldGen() { }
         public override void PostUpdateWorld() {
         }
     }
