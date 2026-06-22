07d9092a2b2e50d97e852ca6d64aef73a8c95c14
Aeris <135448749+Sernk@users.noreply.github.com>
2026-06-22 18:45:46 +0300

    Fix

--- Common/GlobalNPCs/BiomeNPC.cs
+++ Common/GlobalNPCs/BiomeNPC.cs
@@ -1,13 +1,13 @@
 ﻿// Code by SerNik
 using Synergia.Common.Biome;
-using Synergia.Common.GlobalPlayer;
+using Synergia.Content.Buffs;
 using System.Collections.Generic;
 using Terraria;
 
 namespace Synergia.Common.GlobalNPCs {
     public class BiomeNPC : GlobalNPC {
         public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns) {
-            if (player.GetModPlayer<BiomePlayer>().InSnowVillage) { spawnRate = 0; maxSpawns = 0; }
+            if (player.HasBuff<SnowVillageBuff>()) { spawnRate = 0; maxSpawns = 0; }
             GetInstance<EventManger>().Events.TryGetValue(nameof(FrostLegion), out ModEvent mod);
             if (mod.IsActive && Main.invasionX == Main.spawnTileX) {
                 spawnRate = 45;
--- Common/GlobalNPCs/QuestDrawSystem.cs
+++ Common/GlobalNPCs/QuestDrawSystem.cs
@@ -13,24 +13,27 @@ namespace Synergia.Common {
     public partial class QuestSystem {
         public class QuestDrawSystem : GlobalNPC {
             public override bool AppliesToEntity(NPC npc, bool lateInstatiation) => npc.type == NPCType<Dwarf>() || npc.type == NPCID.TaxCollector || npc.type == Roa.Find<ModNPC>("Hunter").Type || npc.type == NPCType<Artist>() || npc.type == NPCType<Ninja>() || npc.type == NPCType<Librarian>() || npc.type == NPCType<HellDwarf>() || npc.type == NPCType<ImperianConsul>();
-            public static void DrawQuestIcon(NPC npc, SpriteBatch spriteBatch, int npcType, string name) {
+            public static void DrawQuestIcon(NPC npc, SpriteBatch spriteBatch, int npcType, string name, int posX = 0, int posY = 0) {
                 if (npc.type != npcType) return;
                 Player player = Main.LocalPlayer;
                 IEnumerable<IQuest> quests = QuestRegistry.GetAvailableQuests(player, name);
                 foreach (IQuest quest in quests) {
                     bool showAvailable = quest.IsAvailable(player) && !quest.IsActive(player);
                     bool showActive = quest.IsActive(player);
-                    quest?.IsActiveQuestUIIcon(showAvailable, showActive, spriteBatch, npc, player);
+                    BaseQuest baseQuest = (BaseQuest)System.Activator.CreateInstance(quest.GetType());
+                    baseQuest.IsActiveQuestUIIconPositionX += posX;
+                    baseQuest.IsActiveQuestUIIconPositionY += posY;
+                    baseQuest?.IsActiveQuestUIIcon(showAvailable, showActive, spriteBatch, npc, player);
                 }
             }
             public override void PostDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
-                DrawQuestIcon(npc, spriteBatch, NPCType<Dwarf>(), DWARF);
+                DrawQuestIcon(npc, spriteBatch, NPCType<Dwarf>(), DWARF, -4);
                 DrawQuestIcon(npc, spriteBatch, NPCID.TaxCollector, TAXC);
-                DrawQuestIcon(npc, spriteBatch, Roa.Find<ModNPC>("Hunter").Type, HUNTER);
-                DrawQuestIcon(npc, spriteBatch, NPCType<Artist>(), ARTIST);
-                DrawQuestIcon(npc, spriteBatch, NPCType<Ninja>(), NINJA);
-                DrawQuestIcon(npc, spriteBatch, NPCType<Librarian>(), LIBRARIAN);
-                DrawQuestIcon(npc, spriteBatch, NPCType<HellDwarf>(), HELLDWARF);
+                DrawQuestIcon(npc, spriteBatch, Roa.Find<ModNPC>("Hunter").Type, HUNTER, - 4, -4);
+                DrawQuestIcon(npc, spriteBatch, NPCType<Artist>(), ARTIST, - 4, -2);
+                DrawQuestIcon(npc, spriteBatch, NPCType<Ninja>(), NINJA, - 4, -2);
+                DrawQuestIcon(npc, spriteBatch, NPCType<Librarian>(), LIBRARIAN, - 4, -2);
+                DrawQuestIcon(npc, spriteBatch, NPCType<HellDwarf>(), HELLDWARF, 0, +2);
                 DrawQuestIcon(npc, spriteBatch, NPCType<ImperianConsul>(), "ImperianConsul");
             }
         }
